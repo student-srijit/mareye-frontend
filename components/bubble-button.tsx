70a93f9a0bce2d8b70bdf0b56f6bd33e9637889f
Srijit Das <srijitd248@gmail.com>
2025-10-16 00:07:27 +0530

    Fix all @/lib/utils imports - Replace with relative imports to resolve module resolution issues

--- components/bubble-button.tsx
+++ components/bubble-button.tsx
@@ -4,7 +4,7 @@ import type React from "react"
 
 import { useState, useRef, forwardRef } from "react"
 import { Button } from "@/components/ui/button"
-import { cn } from "@/lib/utils"
+import { cn } from "../lib/utils"
 
 interface BubbleEffect {
   id: number
